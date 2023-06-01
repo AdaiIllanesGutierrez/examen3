0b135eea18ed5e7afe5f9619c2cff3c394204f41
AdaiIllanesGutierrez <adai.illanes@ucb.edu.bo>
2023-06-01 09:33:11 -0400

    deberia realizar la operacion para devolver el cambio correspondiente

--- src/tienda.js
+++ src/tienda.js
@@ -1,10 +1,14 @@
-function tienda(monto,efectivo) {
+function Tienda(monto,efectivo) {
     let ingresos=[];
     ingresos[0]=monto;
     ingresos[1]=efectivo;
-    
+
     return ingresos;
   }
   
-  export default tienda;
+  function Cambio(billetes) {
+    let cambio= billetes[1]-billetes[0];
+    return cambio;
+  }
+  module.exports = {Tienda,Cambio};
   
--- src/tienda.test.js
+++ src/tienda.test.js
@@ -1,4 +1,4 @@
-import Tienda from "./tienda.js";
+import {Tienda,Cambio} from "./tienda.js";
 
 describe("Ingresar Montos", () => {
   it("deberia ingresar un monto y devolverlo", () => {
@@ -6,5 +6,8 @@ describe("Ingresar Montos", () => {
   });
   it("deberia ingresar un monto de venta y el efectivo y devolver los valores ingresados", () => {
     expect(Tienda(10,20)).toEqual([10,20]);
+  });
+  it("deberia realizar la operacion para devolver el cambio correspondiente", () => {
+    expect(Cambio([10,20])).toEqual(10);
   });
 });
